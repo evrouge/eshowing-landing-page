18b434896a78ac501c7a799e63edf7b7d6a149c7
evin <evinlerouge@icloud.com>
2023-04-10 11:32:05 -0600

    cleaning up the navbar

--- src/App.js
+++ src/App.js
@@ -25,9 +25,9 @@ function App() {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                {/* <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+                {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.2">
                     Another action
@@ -37,12 +37,12 @@ function App() {
                   <NavDropdown.Item href="#action/3.4">
                     Separated link
                   </NavDropdown.Item>
-                </NavDropdown>
+                </NavDropdown> */}
               </Nav>
               <Nav>
-                <Nav.Link href="#deets">More deets</Nav.Link>
+                <Nav.Link href="#deets">How It Works</Nav.Link>
                 <Nav.Link eventKey={2} href="#memes">
-                  Dank memes
+                  Download
                 </Nav.Link>
               </Nav>
             </Navbar.Collapse>
