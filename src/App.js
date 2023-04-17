12844e76de50abecba1ab075bcbfee995ac9fc94
evin <evinlerouge@icloud.com>
2023-04-17 07:22:15 -0600

    changing

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './App.css';
 
 //======BOOTSTRAP IMPORTS===============
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
 
 //========================================
 //========================================
